cab279e1c473611568af0125602a6667ec0539a7
Abhijeet <abhijeetgautam572@gmail.com>
2022-10-27 19:58:26 +0530

    added more CSS

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const title = document.getElementById("title");
 title.textContent = "Metric to Imperial Unit Converter";
 
-const display = document.getElementById("display-container");
-display.textContent = "20";
+const display = document.getElementById("input-container");
+display.value = "20";
+
+const inputNum = document.querySelectorAll(".input-num");
+inputNum.forEach(function(item){
+    item.textContent = "40"
+})
