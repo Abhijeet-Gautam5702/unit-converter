04bb4b5fa9cec6361d7c8ff3aa7508ac08ecf558
Abhijeet <abhijeetgautam572@gmail.com>
2022-10-27 14:45:02 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const title = document.getElementById("title");
+title.textContent = "Metric to Imperial Unit Converter";
+
+const display = document.getElementById("display-container");
+display.textContent = "20";
